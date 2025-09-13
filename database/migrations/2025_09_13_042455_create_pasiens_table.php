f60d808986026189eef21f4b69d2e2d74dcff482
alif232 <aliffahzlian@gmail.com>
2025-09-13 11:30:58 +0700

    Add Seed User

---
+++ database/migrations/2025_09_13_042455_create_pasiens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pasien');
+            $table->string('alamat');
+            $table->string('no_telpon', 20);
+            $table->unsignedBigInteger('rumah_sakit_id');
+            $table->timestamps();
+
+            // Relasi
+            $table->foreign('rumah_sakit_id')
+                  ->references('id')->on('rumah_sakits')
+                  ->onDelete('cascade'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pasiens');
+    }
+};
